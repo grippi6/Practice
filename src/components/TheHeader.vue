25da50126c98c23f41d52289658156d796bc3983
grippi6 <grigorijpiskunov415@gmail.com>
2024-05-27 19:12:32 +0700

    Initial commit

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,30 @@
+<script setup>
+
+</script>
+
+<template>
+  <header >
+<div class="header-start">
+   <router-link :to="{name:'main'}"><img src="../assets/image/лого.png" alt="" width="200px"></router-link>
+  <router-link :to="{name:'OurActor'}"><p>Актёры</p></router-link>
+  <router-link :to="{name:'About'}"><p>О нас</p></router-link>
+</div>
+  </header>
+</template>
+
+<style scoped>
+header{
+width: 100%;
+  background-color: darkred;
+  margin-right: 10em;
+}
+.header-start{
+  align-items: center;
+  display: flex;
+  justify-content: space-evenly;
+}
+p{
+  color:gold;
+  font-size:1.5em;
+}
+</style>
